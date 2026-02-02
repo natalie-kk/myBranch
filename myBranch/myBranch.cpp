7c413e3a7598b22b6d9f72cf4db9340450df77ea
Nataliia_tk <natasha.tkachenko2702@gmail.com>
2026-02-02 23:59:01 +0200

    couts in mybranch

--- myBranch/myBranch.cpp
+++ myBranch/myBranch.cpp
@@ -9,5 +9,8 @@ int main()
     //commit цей стан в mybranch.Переходимо в master, створюємо новій.h файл з будь - яким кодом, робтимо commit.
     //Робимо злиття  mybranch  в master  in VS, вирішуємо конфлікти.Робимо commit в master і push на git.hub.
     //2. Робимо злиття з master в mybranch  in VS, commit, push вітки і приймаєте на git.hub .
+
+    cout << "Some problems have popped up with my branch, but i got how it works\n";
+    cout << "Interesting...\n";
 }
 
