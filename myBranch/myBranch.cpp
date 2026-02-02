b4ec15f0a47bea0137961e655290342030cc1e8b
Nataliia_tk <natasha.tkachenko2702@gmail.com>
2026-02-02 23:40:29 +0200

    mybranch added

--- myBranch/myBranch.cpp
+++ myBranch/myBranch.cpp
@@ -9,5 +9,6 @@ int main()
     //commit цей стан в mybranch.Переходимо в master, створюємо новій.h файл з будь - яким кодом, робтимо commit.
     //Робимо злиття  mybranch  в master  in VS, вирішуємо конфлікти.Робимо commit в master і push на git.hub.
     //2. Робимо злиття з master в mybranch  in VS, commit, push вітки і приймаєте на git.hub .
+    cout << "Hello fron branch!" << endl;
 }
 
