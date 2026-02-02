7fcad6e787fd44952f55de07ac890a79ee1053d6
Nataliia_tk <natasha.tkachenko2702@gmail.com>
2026-02-02 23:27:07 +0200

    Add project files.

---
+++ myBranch/myBranch.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1.Створити репо вашого проекту локально, створити вітку mybranch, в якій пишемо код в main(): привіт з mybranch.
+    //commit цей стан в mybranch.Переходимо в master, створюємо новій.h файл з будь - яким кодом, робтимо commit.
+    //Робимо злиття  mybranch  в master  in VS, вирішуємо конфлікти.Робимо commit в master і push на git.hub.
+    //2. Робимо злиття з master в mybranch  in VS, commit, push вітки і приймаєте на git.hub .
+}
+
